f1ff3fe9ff677f0d402b1548b79e24c8593763fb
imdebamrita <imdebamrita@gmail.com>
2023-09-04 01:03:19 +0530

    make problems section responsive

--- src/components/Problems.jsx
+++ src/components/Problems.jsx
@@ -4,7 +4,7 @@ import dataJson from "../data.json";
 
 export default function Problems() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center text-white">
+    <div className="flex flex-col sm:h-screen h-[calc(180vh)] justify-center items-center text-white">
       <h2 className="text-4xl my-16 sm:my-32">Problem Statements</h2>
       <div className="flex flex-col  sm:flex-row">
         {dataJson.problems.map((e) => {
